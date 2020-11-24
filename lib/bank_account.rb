59433daebc1ed46677dda79454632586e7cbe924
elijahsamuels <elijahsamuels@gmail.com>
2020-11-24 14:29:58 -0700

    commit tree f4b6f222855fefb1c24335a1dd37207a5fe79dc5

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+    
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid? # is valid with an open status and a balance greater than 0
+        self.balance > 0 && self.status == "open"       
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
 class Transfer
-  # your code here
+    
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  # can check that both accounts are valid 
+  # calls on the sender and receiver's #valid? methods
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+      # can execute a successful transaction between two accounts
+      # each transfer can only happen once
+      # rejects a transfer if the sender does not have enough funds (does not have a valid account)
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
+
+  def reverse_transfer
+    if valid? && sender.balance > amount && self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+  
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+
 end
